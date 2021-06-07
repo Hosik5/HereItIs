a9990d8412dfe8514a0d9f4d93784fc2961fd19c
Hosik <83156844+Hosik5@users.noreply.github.com>
2021-06-07 21:36:39 +0900

    CSV file Upload Set
    CSV file Upload Set - 이태희

--- home/admin.py
+++ home/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
+from import_export.admin import ExportActionModelAdmin, ImportExportMixin, ImportMixin
+
+class BoardAdmin(ImportExportMixin, admin.ModelAdmin):
+    pass
 
 from home.models import Search_history
-admin.site.register(Search_history)
+admin.site.register(Search_history,BoardAdmin)
 
 from home.models import Member_info
-admin.site.register(Member_info)
+admin.site.register(Member_info,BoardAdmin)
 
 from home.models import Product_info
-admin.site.register(Product_info)
+admin.site.register(Product_info,BoardAdmin)
 # Register your models here.
+
+
+
+
